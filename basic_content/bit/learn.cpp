18cd32120edfad2028e0b52c6d5c55a942653f34
520yukino <751399893@qq.com>
2023-03-20 00:28:19 +0800

    modified:   basic_content/bit/learn.cpp

--- basic_content/bit/learn.cpp
+++ basic_content/bit/learn.cpp
@@ -6,14 +6,26 @@ struct stuff
     unsigned int field1: 30;
     unsigned int       : 2;
     unsigned int field2: 4;
-    unsigned int       : 0;
-    unsigned int field3: 3; 
+    //unsigned int       : 0; //插入0字段会促使位域自动对齐，即变成3*32字节而非64
+    unsigned int field3: 25; 
 };
-int main(){
-    struct stuff s={1,3,5};
-    cout<<s.field1<<endl;
-    cout<<s.field2<<endl;
-    cout<<s.field3<<endl;
-    cout<<sizeof(s)<<endl;
-    return 0;
+union u1
+{
+    stuff s;
+    unsigned long long a;
+};
+
+int main()
+{
+    u1 u={.s={11111,3,8}};
+    cout<<u.s.field1<<endl;
+    cout<<u.s.field2<<endl;
+    cout<<u.s.field3<<endl;
+    cout<<sizeof(u)<<endl;
+
+    u.a = 0;
+    cout<<u.s.field1<<endl;
+    cout<<u.s.field2<<endl;
+    cout<<u.s.field3<<endl;
+    cout<<sizeof(u)<<endl;
 }
