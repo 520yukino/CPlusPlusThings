dba7610cce00f39d780ce829e2fd73d60aac768f
520yukino <751399893@qq.com>
2023-03-24 15:46:29 +0800

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   basic_content/bit/learn.exe 	new file:   basic_content/extern/extern_c++/CMakeLists.txt 	modified:   basic_content/extern/extern_c++/add.cpp 	modified:   basic_content/extern/extern_c++/add.h 	modified:   basic_content/extern/extern_c++/add.o 	deleted:    basic_content/extern/extern_c++/main 	new file:   basic_content/extern/extern_c++/main.exe

--- basic_content/extern/extern_c++/add.cpp
+++ basic_content/extern/extern_c++/add.cpp
@@ -3,7 +3,12 @@ using namespace std;
 extern "C" {
     #include "add.h"
 }
+/* int add(int x) {
+    puts("C++\n");
+    return x;
+} */
 int main() {
     add(2,3);
+    getchar();
     return 0;
 }
--- basic_content/extern/extern_c++/add.h
+++ basic_content/extern/extern_c++/add.h
@@ -1,4 +1,4 @@
 #ifndef ADD_H
 #define ADD_H
-extern int add(int x,int y);
+int add(int x,int y);
 #endif
